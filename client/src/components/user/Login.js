b2ff9301e158d6d70ad71bafde2132d7a8204b20
Jordan Halfman <jordanhalfman@gmail.com>
2023-02-16 10:59:36 -0700

    create state to monitor form input; update values based on state

--- client/src/components/user/Login.js
+++ client/src/components/user/Login.js
@@ -1,33 +1,50 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../context/user.js';
+import React, { useState } from 'react';
 
 const Login = () => {
-  const user = useContext(UserContext);
+  const [newUserForm, setNewUserForm] = useState({
+    email: "",
+    username: "",
+    password: "",
+    password_confirmation: ""
+  })
+
+  function editUserForm(e) {
+    setNewUserForm({
+      ...newUserForm,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  function createNewUser(e) {
+    e.preventDefault();
+    console.log(newUserForm)
+  }
   
   return (
-      <form>
+      <form onSubmit={createNewUser}>
         <div className="form-floating mb-3">
-          <input type="email" className="form-control" id="email" placeholder="name@email.com"/>
-          <label for="email" className="form-label">Email Address</label>
+          <input type="email" className="form-control" id="email" placeholder="name@email.com" onChange={editUserForm} value={newUserForm.email}/>
+          <label htmlFor="email" className="form-label">Email Address</label>
         </div>
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" id="username" placeholder="username"/>
-          <label for="username" className="form-label">Username</label>
+          <input type="text" className="form-control" id="username" placeholder="username" onChange={editUserForm} value={newUserForm.username}/>
+          <label htmlFor="username" className="form-label">Username</label>
         </div>
         <div className="form-floating mb-3">
-          <input type="password" className="form-control" id="password" aria-describedby="passwordHelp" placeholder="password"/>
-          <label for="password" className="form-label">Password</label>
-          <div id="passwordHelp" className="form-text">Must be at least 8-15 characters with one special character</div>
+          <input type="password" className="form-control" id="password" aria-describedby="passwordHelp" placeholder="password" onChange={editUserForm} value={newUserForm.password}/>
+          <label htmlFor="password" className="form-label">Password</label>
+          <div id="passwordHelp" className="form-text" style={{textAlign: "left"}}>Must be at least 8-15 characters with one special character</div>
         </div>
         <div className="form-floating mb-3">
-          <input type="password" className="form-control" id="passwordConfirmation" placeholder="password"/>
-          <label for="passwordConfirmation" className="form-label">Password Confirmation</label>
+          <input type="password" className="form-control" id="password_confirmation" placeholder="password" onChange={editUserForm} value={newUserForm.password_confirmation}/>
+          <label htmlFor="passwordConfirmation" className="form-label">Password Confirmation</label>
+          <div id="passwordConfirmationHelp" className="form-text" style={{textAlign: "left"}}>Must be at least 8-15 characters with one special character</div>
         </div>
         {/* <div className="mb-3 form-check">
           <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
           <label className="form-check-label" for="exampleCheck1">Keep me signed in</label>
         </div> */}
-        <button type="submit" className="btn btn-primary">Create New Account</button>
+        <button type="submit" className="btn btn-primary" style={{float: "left"}}>Create New Account</button>
       </form>
   )
 }
