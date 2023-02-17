dcdff6487bbc10e30d532d2c704b3583e1b0ba45
Jordan Halfman <jordanhalfman@gmail.com>
2023-02-16 21:24:22 -0700

    added login/logout toggle with logout button

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink as Link} from 'react-router-dom';
+import { UserContext } from '../context/user.js';
 
 const Navbar = () => {
+    const {user, setUser} = useContext(UserContext);
+
+    function logout() {
+        fetch(`/logout`, {
+            method: "DELETE"
+          })
+        setUser(null)
+    }
+
   return (
     <nav className="navbar navbar-expand-lg bg-primary">
         <div className="container-fluid">
-            <Link to="/" className="navbar-brand link-light">Pet Diver<img style={{marginLeft: "10px"}}src='https://i.imgur.com/YC6rhTp.png' alt="Logo" width="50" height="40" class="d-inline-block align-text-top"></img></Link>
+            <Link to="/characters" className="navbar-brand link-light">Pet Diver<img style={{marginLeft: "10px"}}src='https://i.imgur.com/YC6rhTp.png' alt="Logo" width="50" height="40" class="d-inline-block align-text-top"></img></Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -18,7 +28,7 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-nav ms-auto">
                     <Link to="/newuser" className="nav-link link-light">Create Account</Link>
-                    <Link to="/login" className="nav-link link-light">Login</Link>
+                    {!user ? <Link to="/login" className="nav-link link-light">Login</Link> : <Link onClick={logout} to="/login" className="nav-link link-light">Logout</Link>}
                 </div>
             </div>
         </div>
