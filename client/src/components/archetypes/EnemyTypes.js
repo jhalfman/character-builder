83fc2bd149af6dbb340ae82b31452ccbce3bed6b
Jordan Halfman <jordanhalfman@gmail.com>
2023-02-22 08:09:58 -0700

    add grid for pet archetype display; updated attack_modifier in previous commit to correct spelling for enemy archetype model

--- client/src/components/archetypes/EnemyTypes.js
+++ client/src/components/archetypes/EnemyTypes.js
@@ -11,18 +11,18 @@ const EnemyTypes = () => {
 
   return (
     <div className="container">
-        <div className="row border border-primary">
-            <div className="col-sm-12 border border-secondary"><h1 style={{textAlign: "center", marginTop: "10px"}}>Enemies</h1></div>
+        <div className="row">
+            <div className="col-sm-12"><h1 style={{textAlign: "center", marginTop: "10px"}}>Enemy Types</h1></div>
             {enemies ? enemies.map(enemy => {
                 return (
-                    <div className="col-sm-4 border border-secondary" key={enemy.name} style={{marginTop: "20px", paddingRight: "20px", paddingLeft: "20px"}}> 
-                        <div className="row border border-success justify-content-center">
+                    <div className="col-sm-4 border border-primary" key={enemy.name} style={{marginTop: "20px", paddingRight: "20px", paddingLeft: "20px"}}> 
+                        <div className="row justify-content-center">
                             <h3 style={{textAlign: "center"}}>{enemy.name}</h3>
                         </div>
-                        <div className="row border border-success justify-content-center"> 
+                        <div className="row justify-content-center"> 
                             <img src={`${enemy.image_url}`} style={{width: "200px"}}></img>
                         </div>
-                        <div className="row border border-success">
+                        <div className="row">
                             <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
                             <ul style={{marginLeft: "10px", marginTop: "-10px"}}>
                                 <li>Attack: {Math.trunc(enemy.attack_modifier * 100)}%</li>
--- client/src/components/archetypes/PetTypes.js
+++ client/src/components/archetypes/PetTypes.js
@@ -10,7 +10,31 @@ const PetTypes = () => {
     }, [])
 
   return (
-    <div>{pets ? pets.map(pet => <img src={`${pet.image_url}`} style={{width: "50px"}}/>) : null}</div>
+    <div className="container">
+        <div className="row">
+            <div className="col-sm-12 "><h1 style={{textAlign: "center", marginTop: "10px"}}>Pet Types</h1></div>
+            {pets ? pets.map(pet => {
+                return (
+                    <div className="col-sm-4 border border-primary" key={pet.name} style={{marginTop: "20px", paddingRight: "20px", paddingLeft: "20px"}}> 
+                        <div className="row justify-content-center">
+                            <h3 style={{textAlign: "center"}}>{pet.name}</h3>
+                        </div>
+                        <div className="row justify-content-center"> 
+                            <img src={`${pet.image_url}`} style={{width: "200px"}}></img>
+                        </div>
+                        <div className="row">
+                            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                            <ul style={{marginLeft: "10px", marginTop: "-10px"}}>
+                                <li>Attack: {Math.trunc(pet.attack * 100)}%</li>
+                                <li>Defense: {Math.trunc(pet.defense * 100)}%</li>
+                                <li>Speed: {Math.trunc(pet.speed * 100)}%</li>
+                            </ul>
+                        </div>
+                    </div>
+                )
+            }) : null}
+        </div>
+    </div>
   )
 }
 
