9535d3f3e59cd5c1bed737c7a59105266e8d0846
Jordan Halfman <jordanhalfman@gmail.com>
2023-03-23 00:07:01 -0700

    URL for host and domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "postgres://jordan_db_r94h_user:GGmepu1Pd8ePKdpi7TP6zCo6Ylqe5Xc7@dpg-cfltd1la499b93e0bic0-a.oregon-postgres.render.com/jordan_db_r94h" }
+  config.action_mailer.default_url_options = { host: "https://pet-diver.onrender.com/" }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     user_name:            ENV["EMAIL"],
     password:             ENV["EMAILPASSWORD"],
     authentication:       'plain',
+    domain:               'render.com',
     enable_starttls_auto: true
   }
 
